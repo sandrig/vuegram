302416917ece1f5e153bd5108c951f617e4d7eca
Andrey Savelyev <sandrig.dev@gmail.com>
2019-07-01 23:47:29 +0300

    feat: add upload image

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,33 @@
   <div id="app">
     <div class="app-phone">
       <div class="phone-header">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/vue_gram_logo_cp.png" />
+        <img
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/vue_gram_logo_cp.png"
+        />
       </div>
       <phone-body
+        :step="step"
         :posts="posts"
-        :filters="filters" />
+        :filters="filters"
+        :image="image"
+        :selectedFilter="selectedFilter"
+        v-model="caption"
+      />
       <div class="phone-footer">
         <div class="home-cta">
           <i class="fas fa-home fa-lg"></i>
         </div>
         <div class="upload-cta">
-          <i class="far fa-plus-square fa-lg"></i>
+          <input
+            type="file"
+            name="file"
+            id="file"
+            class="inputfile"
+            @change="uploadImage"
+          />
+          <label for="file">
+            <i class="far fa-plus-square fa-lg"></i>
+          </label>
         </div>
       </div>
     </div>
@@ -20,25 +36,45 @@
 </template>
 
 <script>
-  import PhoneBody from "./components/PhoneBody";
+import PhoneBody from './components/PhoneBody';
 
-  import posts from "./data/posts";
-  import filters from "./data/filters";
+import posts from './data/posts';
+import filters from './data/filters';
 
-  export default {
-    name: "App",
-    data() {
-      return {
-        posts,
-        filters,
+export default {
+  name: 'App',
+  data() {
+    return {
+      step: 1,
+      posts,
+      filters,
+      image: '',
+      selectedFilter: '',
+      caption: '',
+    };
+  },
+  methods: {
+    uploadImage(evt) {
+      const files = evt.target.files;
+      if (!files.length) return;
+
+      const reader = new FileReader();
+      reader.readAsDataURL(files[0]);
+      reader.onload = evt => {
+        this.image = evt.target.result;
+        this.step = 2;
       };
+
+      // To enable reuploading of same files in Chrome
+      document.querySelector('#file').value = '';
     },
-    components: {
-      "phone-body": PhoneBody
-    }
-  };
+  },
+  components: {
+    'phone-body': PhoneBody,
+  },
+};
 </script>
 
 <style lang="scss" src="./styles/app.scss">
-  // Styles from stylesheet
+// Styles from stylesheet
 </style>
