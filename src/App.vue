8d793c456975d1271b979a6368fadffd58e27977
Andrey Savelyev <sandrig.dev@gmail.com>
2019-07-04 22:02:51 +0300

    refactor: prettify

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,7 @@
   <div id="app">
     <div class="app-phone">
       <div class="phone-header">
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/vue_gram_logo_cp.png"
-        />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/vue_gram_logo_cp.png" />
       </div>
       <phone-body
         :step="step"
@@ -24,8 +22,7 @@
             name="file"
             id="file"
             class="inputfile"
-            @change="uploadImage"
-          />
+            @change="uploadImage"/>
           <label for="file">
             <i class="far fa-plus-square fa-lg"></i>
           </label>
@@ -36,45 +33,45 @@
 </template>
 
 <script>
-import PhoneBody from './components/PhoneBody';
+  import PhoneBody from "./components/PhoneBody";
 
-import posts from './data/posts';
-import filters from './data/filters';
+  import posts from "./data/posts";
+  import filters from "./data/filters";
 
-export default {
-  name: 'App',
-  data() {
-    return {
-      step: 1,
-      posts,
-      filters,
-      image: '',
-      selectedFilter: '',
-      caption: '',
-    };
-  },
-  methods: {
-    uploadImage(evt) {
-      const files = evt.target.files;
-      if (!files.length) return;
-
-      const reader = new FileReader();
-      reader.readAsDataURL(files[0]);
-      reader.onload = evt => {
-        this.image = evt.target.result;
-        this.step = 2;
+  export default {
+    name: "App",
+    data() {
+      return {
+        step: 1,
+        posts,
+        filters,
+        image: "",
+        selectedFilter: "",
+        caption: ""
       };
-
-      // To enable reuploading of same files in Chrome
-      document.querySelector('#file').value = '';
     },
-  },
-  components: {
-    'phone-body': PhoneBody,
-  },
-};
+    methods: {
+      uploadImage(evt) {
+        const files = evt.target.files;
+        if (!files.length) return;
+
+        const reader = new FileReader();
+        reader.readAsDataURL(files[0]);
+        reader.onload = evt => {
+          this.image = evt.target.result;
+          this.step = 2;
+        };
+
+        // To enable reuploading of same files in Chrome
+        document.querySelector("#file").value = "";
+      }
+    },
+    components: {
+      "phone-body": PhoneBody
+    }
+  };
 </script>
 
 <style lang="scss" src="./styles/app.scss">
-// Styles from stylesheet
+  // Styles from stylesheet
 </style>
